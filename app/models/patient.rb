17540518ecf2196f1ef349e1c25ebe931460bdde
Abdul Barek <barek2k2@gmail.com>
2026-07-13 08:54:55 -0600

    audit log handled for sensitive data

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,11 @@
 class Patient < ApplicationRecord
-  audited
+  audited except: %i[ssn dob]
   encrypts :ssn
+
+  def masked_ssn
+    return if ssn.blank?
+
+    "***-**-#{ssn.to_s.last(4)}"
+  end
+
 end
