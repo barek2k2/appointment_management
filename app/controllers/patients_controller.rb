ebdb61a939de25d446341e631cd4c570156d7470
Abdul <barek2k2@gmail.com>
2026-07-24 18:57:07 +0600

    authentication implemented

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,22 +1,23 @@
 class PatientsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_patient, only: %i[ show edit update destroy ]
 
   def index
-    @patients = Patient.all
+    @patients = current_user.patients
   end
 
   def show
   end
 
   def new
-    @patient = Patient.new
+    @patient = current_user.patients.new
   end
 
   def edit
   end
 
   def create
-    @patient = Patient.new(patient_params)
+    @patient = current_user.patients.new(patient_params)
 
     respond_to do |format|
       if @patient.save
@@ -52,7 +53,7 @@ class PatientsController < ApplicationController
 
   private
     def set_patient
-      @patient = Patient.find(params[:id])
+      @patient = current_user.patients.find(params[:id])
     end
 
     def patient_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_24_122620) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_24_125410) do
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
     t.string "ssn"
     t.string "soccer_team_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
