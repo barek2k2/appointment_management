fd66faf7d87860aa6f2e7bb987e80a5a024bfb94
Abdul <barek2k2@gmail.com>
2026-07-24 18:31:24 +0600

    comments removed

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,25 +1,20 @@
 class PatientsController < ApplicationController
   before_action :set_patient, only: %i[ show edit update destroy ]
 
-  # GET /patients or /patients.json
   def index
     @patients = Patient.all
   end
 
-  # GET /patients/1 or /patients/1.json
   def show
   end
 
-  # GET /patients/new
   def new
     @patient = Patient.new
   end
 
-  # GET /patients/1/edit
   def edit
   end
 
-  # POST /patients or /patients.json
   def create
     @patient = Patient.new(patient_params)
 
@@ -34,7 +29,6 @@ class PatientsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /patients/1 or /patients/1.json
   def update
     respond_to do |format|
       if @patient.update(patient_params)
@@ -47,7 +41,6 @@ class PatientsController < ApplicationController
     end
   end
 
-  # DELETE /patients/1 or /patients/1.json
   def destroy
     @patient.destroy!
 
@@ -58,12 +51,10 @@ class PatientsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_patient
       @patient = Patient.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def patient_params
       params.require(:patient).permit(:first_name, :ssn, :soccer_team_name)
     end
