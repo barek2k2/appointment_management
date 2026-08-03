3cacdfd5380fdeadf0f31ef75ebac270758e2348
Abdul <barek2k2@gmail.com>
2026-08-03 09:55:11 -0500

    sql injection fixed

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,8 +2,6 @@ class PatientsController < ApplicationController
     before_action :authenticate_user!
 
     def search
-    @patients = current_user.patients.where(
-      "name = '#{params[:name]}'"
-    )
+    @patients = current_user.patients.where(name: params[:name])
   end
 end
