81bb6674354be4b4dcf7679c0b87ca5d1d9acf44
Abdul <barek2k2@gmail.com>
2026-08-03 10:57:10 -0500

    relation and audit added

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,6 +2,12 @@ class PatientsController < ApplicationController
     before_action :authenticate_user!
 
     def search
-    @patients = current_user.patients.where(name: params[:name])
-  end
+      @patients = current_user.patients.where(name: params[:name])
+      Audit.create!(
+      action: "search",
+      comment: "Patient record searched",
+      user: current_user,
+      audited_changes: {}
+     )
+    end
 end
